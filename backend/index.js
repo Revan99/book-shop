961500f167d6b37036b771c8e017090fa6c4cd25
Revan Sarbast Salih <revansarbast8@gmail.com>
2022-10-15 10:01:32 +0300

    Remove ignored files

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,27 @@ const db = mysql.createConnection({
 
 const app = express();
 
+app.get("/", (req, res) => {
+  return res.status(200).send("Hello world");
+});
+
+app.get("/books", (req, res) => {
+  const query = "SELECT * FROM books";
+  db.query(query, (err, data) => {
+    if (err) return res.status(500).send("something went wrong");
+    return res.status(200).json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const query = "INSET INTO book (`title`, `desc`, `cover`) VALUES(?)";
+  const values = ["title", "desc", "cover"];
+  db.query(query, [values], (err, data) => {
+    if (err) return res.status(500).send("something went wrong");
+    return res.status(200).json(data);
+  });
+});
+
 app.listen(8080, () => {
   console.log("server is running");
 });
